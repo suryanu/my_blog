03560752095015258f37d29355a2fcb5efba98bd
Surya Bhaskar Reddy <suryabhaskarreddy@yahoo.in>
2016-09-20 22:48:21 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+// for smooth scroll
+$(function() {
+	"use strict";
+  $('nav li a:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+//-- end of smooth scroll
+
+// nav-bar color change.
